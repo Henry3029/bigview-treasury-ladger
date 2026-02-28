50c3851318fdf884bd5c764764a35ba91c1b3285
Henry goodluck <chigoziehenry852@gmail.com>
2026-02-28 16:04:48 +0100

    remove pages folder

--- layout.tsx
+++ layout.tsx
@@ -1,4 +1,5 @@
 // app/layout.tsx
+import './globals.css'
 import AppPrivyProvider from './components/PrivyProvider';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
