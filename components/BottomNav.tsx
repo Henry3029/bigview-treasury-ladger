59608022379d3fc4848f13105dd34256ea975994
Henry goodluck <chigoziehenry852@gmail.com>
2026-03-03 19:29:54 +0100

    feat: add rewards components

---
+++ components/BottomNav.tsx
@@ -0,0 +1,31 @@
+// components/BottomNav.tsx
+"use client";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function BottomNav() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around p-3 pb-6">
+      <Link href="/" className={pathname === '/' ? 'text-blue-600' : 'text-gray-400'}>
+        <div className="flex flex-col items-center">
+          <span>🏠</span>
+          <span className="text-xs">Home</span>
+        </div>
+      </Link>
+      <Link href="/stake" className={pathname === '/stake' ? 'text-blue-600' : 'text-gray-400'}>
+        <div className="flex flex-col items-center">
+          <span>🥩</span>
+          <span className="text-xs">Stake</span>
+        </div>
+      </Link>
+      <Link href="/rewards" className={pathname === '/rewards' ? 'text-blue-600' : 'text-gray-400'}>
+        <div className="flex flex-col items-center">
+          <span>🎁</span>
+          <span className="text-xs">Rewards</span>
+        </div>
+      </Link>
+    </nav>
+  );
+}
