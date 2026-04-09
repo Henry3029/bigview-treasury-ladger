c08fdf8b0cf95c744cd8d3dffe6ac28fdd2d66bf
Henry goodluck <chigoziehenry852@gmail.com>
2026-04-09 15:07:32 +0100

    refactor: implement MobileHeaderWrapper to resolve server-side event handler error

---
+++ src/components/MobileHeaderWrapper.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React, { useState } from 'react';
+import MobileHeader from './MobileHeader';
+import NotificationDropdown from './NotificationDropdown';
+
+export default function MobileHeaderWrapper() {
+  const [isNotifOpen, setIsNotifOpen] = useState(false);
+
+  return (
+    <>
+      <MobileHeader onNotificationClick={() => setIsNotifOpen(true)} />
+      
+      {/* If you have a NotificationDropdown, it lives here now */}
+      <NotificationDropdown 
+        isOpen={isNotifOpen} 
+        onClose={() => setIsNotifOpen(false)} 
+      />
+    </>
+  );
+}
