4ca55876d8f63486085360dd320ed0d9019012b7
Henry goodluck <chigoziehenry852@gmail.com>
2026-04-10 08:06:39 +0100

    modify notification file

--- src/components/NotificationDropdown.tsx
+++ src/components/NotificationDropdown.tsx
@@ -48,7 +48,7 @@ export default function NotificationDropdown({
 
       {/* 3. NOTIFICATION LIST: High-Contrast Dark Theme */}
       <div className="flex-1 overflow-y-auto p-4 space-y-3">
-        {notifications.length > 0 ? (
+        {notifications?.length > 0 ? (
           notifications.map((n) => (
             <div 
               key={n.id} 
