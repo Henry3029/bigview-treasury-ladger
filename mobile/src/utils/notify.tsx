6e6fbab5185b0bb3c42755796e5773f06097a163
Henry goodluck <chigoziehenry852@gmail.com>
2026-05-08 08:40:44 +0100

    create notify.tsx file

---
+++ mobile/src/utils/notify.tsx
@@ -0,0 +1,22 @@
+import React from 'react'; 
+import {View, Text} from 'react-native'; 
+
+type statusType = 'success' | 'error' | 'info'; 
+
+export const Notify = (message: string, status: statusType): string => {
+
+if(!message) return "null";
+
+const getStatusColor = () => {
+if(status === 'success') return 'green';
+if(status === 'error') return 'red'; 
+return 'blue'; // default for info.
+};
+
+return (
+<View style={{padding: 10, backgroundColor: '#fff', borderRadius: 8}}>
+<Text style={{color: getStatusColor(),  fontWeight: 'bold'}}>{message}
+</Text>
+</View>
+);
+};
