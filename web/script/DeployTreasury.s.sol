cfe87acbf10e57f08d7407e19953f36b8bfca355
Henry goodluck <chigoziehenry852@gmail.com>
2026-05-23 13:14:24 +0100

    t

--- web/script/DeployTreasury.s.sol
+++ web/script/DeployTreasury.s.sol
@@ -15,15 +15,9 @@ contract DeployTreasury is Script {
 
         // 3. Deploy the contract passing the arguments directly in clean Solidity math
         new BigViewTreasury(
-<<<<<<< HEAD
             0x28D549A2517FCB78f634088999aF99EED055ab86, // Your Token
             0x787Ba86c560569c879b93350Ca72C277E2134e1D, // cbETH
             0xa24112B301B9eb4475633523590DBd0e591D3C56  // Your Deployer Pool
-=======
-            0x28D549A2517FCB78f63408899af99EED055ab86, // Exact token checksum
-            0x787Ba86c560569c879b93350Ca72C277E2134e1D, // Exact cbETH address
-            0xa24112B301B9eb4475633523590DBd0e591D3C56,
->>>>>>> 00e13b0e03838a958206dfcb4f4b2b3baf494a8b
         );
 
         vm.stopBroadcast();
