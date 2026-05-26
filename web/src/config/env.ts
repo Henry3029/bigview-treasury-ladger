4f80197b26cb8c9a2bed06f886139c38c5b0d99f
Henry goodluck <chigoziehenry852@gmail.com>
2026-05-26 20:35:07 +0100

    modify env config file

--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -14,7 +14,7 @@ const rawDeployer = process.env.NEXT_PUBLIC_DEPLOYER_ADDRESS.toLowerCase();
 const ZERO_ADDRESS: `0x${string}` = "0x0000000000000000000000000000000000000000";
 
 export const TREASURY_ADDRESS = (rawTreasury as `0x${string}`) || ZERO_ADDRESS;
-export const TOKEN_ADDRESS = (rawGovernanceToken as `0x${string}`) || ZERO_ADDRESS;
+export const TOKEN_ADDRESS = (rawToken as `0x${string}`) || ZERO_ADDRESS;
 export const CBETH_TOKEN_ADDRESS = (rawCbEth as `0x${string}`) || ZERO_ADDRESS;
 export const DEPLOYER_ADDRESS = (rawDeployer as `0x${string}`) || ZERO_ADDRESS;
 
@@ -24,7 +24,7 @@ export const DEPLOYER_ADDRESS = (rawDeployer as `0x${string}`) || ZERO_ADDRESS;
 // ==========================================
 
 export const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID || "missing-privy-id";
-export const ALCHEMY_RPC_URL = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/...";
+export const RPC_URL = process.env.NEXT_PUBLIC_BASE_SEPOLIA_RPC || "https://eth-sepolia.g.alchemy.com/v2/...";
 
 // Convert string numbers from .env into actual numbers safely
 export const CHAIN_ID = Number(process.env.NEXT_PUBLIC_CHAIN_ID) || 11155111; // Defaults to Sepolia
