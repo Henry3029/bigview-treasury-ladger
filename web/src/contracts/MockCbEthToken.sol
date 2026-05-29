a00b4299c535ab5499e0cdbb9ec63e92930b5167
Henry goodluck <chigoziehenry852@gmail.com>
2026-05-29 06:41:39 +0100

    modify mock file

--- web/src/contracts/MockCbEthToken.sol
+++ web/src/contracts/MockCbEthToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// 🚀 Deploy this as your temporary twin test asset!
+// Deploy this as your temporary twin test asset!
 contract MockCbETH is ERC20 {
     constructor() ERC20("Mock Coinbase Wrapped Staked ETH", "cbETH") {
         _mint(msg.sender, 1000000 * 10**18); // Mint test tokens to yourself
