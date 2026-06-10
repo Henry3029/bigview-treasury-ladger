ebc7df161a4e7057ddaed0aba52d9780f813e3b9
Henry chigozie Goodluck <chigoziehenry852@gmail.com>
2026-06-10 01:02:06 +0100

    testing

--- web/src/contracts/BigViewTreasury.sol
+++ web/src/contracts/BigViewTreasury.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.20;
 
 // 1. FIXED: Added '-upgradeable' to the root package name
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+// Change your imports to use this specific curly-brace format:
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+
+
 // 2. This matches your line perfectly!
 contract BigViewTreasury is Initializable, ReentrancyGuardUpgradeable {
 
