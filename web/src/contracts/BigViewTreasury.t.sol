3f4b887f22341d1372c2b1e7f36a49ad1c94b443
Henry chigozie Goodluck <chigoziehenry852@gmail.com>
2026-06-19 08:08:34 +0100

    testing

--- web/src/contracts/BigViewTreasury.t.sol
+++ web/src/contracts/BigViewTreasury.t.sol
@@ -98,7 +98,7 @@ address user = address(1);
 // 🧪 TEST 2: Partial Unstake (Withdrawing a fraction)
     function test_PartialUnstake() public {
         // Start with 100 ether in the contract vault
-        deal(address(user1), 110 ether);
+        deal(address(user1), 160 ether);
 
 // 2. Pretend to be user1 and STAKE the 100 ether into the contract
     // (This automatically fills the contract vault AND updates the struct state!)
